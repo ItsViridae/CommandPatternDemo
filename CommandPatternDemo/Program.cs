fba18eb5bb8d415243fa4356fb8e98694d9d00b5
Scott Comeaux <viridaecode@gmail.com>
2019-08-05 19:18:24 -0500

    Add project files.

---
+++ CommandPatternDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
